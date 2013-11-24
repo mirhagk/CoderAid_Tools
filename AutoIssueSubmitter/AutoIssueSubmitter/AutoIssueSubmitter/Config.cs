3bf3c53e8173fb8cbdea64afe26f6127c25c79a1
Nathan Jervis <mirhagk@gmail.com>
2013-11-23 19:19:08 -0500

    Added Config
    Will load it's settings from config.json

---
+++ AutoIssueSubmitter/AutoIssueSubmitter/AutoIssueSubmitter/Config.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace AutoIssueSubmitter
+{
+    [Serializable]
+    class Config
+    {
+        public class Credentials
+        {
+            public string Username { get; set; }
+            public string Password { get; set; }
+        }
+        static public Config config;
+        static Config()
+        {
+            config = LoadConfig("config.json");
+        }
+        public static Config LoadConfig(string filename)
+        {
+            return JsonConvert.DeserializeObject<Config>(System.IO.File.ReadAllText(filename));
+        }
+        public System.Net.NetworkCredential NetworkCredential
+        {
+            get
+            {
+                return new System.Net.NetworkCredential(credentials.Username, credentials.Password);
+            }
+        }
+        public Credentials credentials { get; set; }
+    }
+}
